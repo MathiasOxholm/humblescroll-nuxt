cf05e27a1db0ba7c22ff193e8c626f317a28d302
Mathias Oxholm <mox@novicell.dk>
2023-08-29 00:04:16 +0200

    Prepare for release

--- playground/app.vue
+++ playground/app.vue
@@ -25,331 +25,333 @@
           <h1 class="font-semibold text-7xl">
             HumbleScroll
           </h1>
-          <p class="mt-8 text-xl">A Vue 3 component for scroll animations.</p>
+          <p class="mt-8 text-xl">
+            A Vue 3 component for scroll animations.
+          </p>
         </HumbleScroll>
       </div>
     </section>
     <div class="container">
       <div class="grid gap-20 px-6 pb-8 lg:grid-cols-12 lg:px-8">
         <div class="lg:col-span-9">
-          <Section 
+          <TheSection 
             id="fade"
             title="Fade animations"
             description="Opacity from 0 to 1 - you know the drill."
           >
-            <Card
+            <TheCard
               title="Fade up"
               animation="fade up lg:left lg:blur"
             />
-            <Card
+            <TheCard
               title="Fade down"
               animation="fade down"
             />
-            <Card
+            <TheCard
               title="Fade right"
               animation="fade right"
             />
-            <Card
+            <TheCard
               title="Fade left"
               animation="fade left"
             />
-            <Card
+            <TheCard
               title="Fade in"
               animation="fade"
             />
-            <Card
+            <TheCard
               title="Fade up left"
               animation="fade up left"
             />
-          </Section>
+          </TheSection>
 
-          <Section
+          <TheSection
             id="zoom"
             title="Zoom animations"
             description="Zoom in and out. Perfect for images."
           >
-            <Card
+            <TheCard
               title="Zoom in"
               animation="zoom-in fade"
             />
-            <Card
+            <TheCard
               title="Zoom out"
               animation="zoom-out fade"
             />
-            <Card
+            <TheCard
               title="Zoom out + right + up"
               animation="zoom-out right up fade"
             />
-            <Card
+            <TheCard
               title="Zoom in + left + up"
               animation="zoom-in left up fade"
             />
-          </Section>
+          </TheSection>
           
-          <Section
+          <TheSection
             id="slide"
             title="Slide animations"
             description="Slide to the left, slide it to the right!"
           >
-            <Card
+            <TheCard
               title="Slide right"
               animation="right"
             />
-            <Card
+            <TheCard
               title="Slide left"
               animation="left"
             />
-            <Card
+            <TheCard
               title="Slide up"
               animation="right up"
             />
-            <Card
+            <TheCard
               title="Slide down"
               animation="left up"
             />
-          </Section>
+          </TheSection>
 
-          <Section
+          <TheSection
             id="flip"
             title="Flip animations"
             description="Who uses these? Here's four of them anyway."
           >
-            <Card
+            <TheCard
               title="Flip right"
               animation="flip-right"
             />
-            <Card
+            <TheCard
               title="Flip left"
               animation="flip-left"
             />
-            <Card
+            <TheCard
               title="Flip up"
               animation="flip-up"
             />
-            <Card
+            <TheCard
               title="Flip down"
               animation="flip-down"
             />
-          </Section>
+          </TheSection>
 
-          <Section
+          <TheSection
             id="skew"
             title="Skew animations"
             description="Skew it to the left, skew it to the right! Combine with blur to make them feel blazingly fast."
           >
-            <Card
+            <TheCard
               title="Skew right"
               animation="skew-right fade right blur"
               speed="fast"
               easing="ease-out-back"
               size="extra-large"
             />
-            <Card
+            <TheCard
               title="Skew left"
               animation="skew-left fade left blur"
               speed="fast"
               easing="ease-out-back"
               size="extra-large"
             />
-            <Card
+            <TheCard
               title="Skew up"
               animation="skew-up fade up blur"
               speed="fast"
               easing="ease-out-back"
               size="extra-large"
             />
-            <Card
+            <TheCard
               title="Skew down"
               animation="skew-down fade down blur"
               speed="fast"
               easing="ease-out-back"
               size="extra-large"
             />
-          </Section>
+          </TheSection>
 
-          <Section
+          <TheSection
             id="reveal"
             title="Reveal animations"
             description="Parent has overflow hidden and child slides from 100% to 0"
           >
-            <Card
+            <TheCard
               title="Reveal up"
               animation="reveal-up fade"
             />
-            <Card
+            <TheCard
               title="Reveal down"
               animation="reveal-down fade"
             />
-            <Card
+            <TheCard
               title="Reveal right"
               animation="reveal-right fade"
             />
-            <Card
+            <TheCard
               title="Reveal left"
               animation="reveal-left fade"
             />
-          </Section>
+          </TheSection>
 
-          <Section
+          <TheSection
             id="blur"
             title="Blur effect"
             description="Who doesn't like a good motion blur effect?"
             :columns="3"
           >
-            <Card
+            <TheCard
               title="Blur effect"
               animation="fade right blur"
             />
-            <Card
+            <TheCard
               title="Blur effect"
               animation="zoom-in fade up blur"
               :variables="{ blur: '40px' }"
             />
-            <Card
+            <TheCard
               title="Blur effect"
               animation="fade left blur"
             />
-          </Section>
+          </TheSection>
           
-          <Section
+          <TheSection
             id="easing"
             title="Easing variations"
             description="We only ship with a few easings, but you can easily add your own."
           >
-            <Card
+            <TheCard
               title="Ease in"
               animation="fade up"
               easing="ease-in"
             />
-            <Card
+            <TheCard
               title="Ease out"
               animation="fade up"
               easing="ease-out"
             />
-            <Card
+            <TheCard
               title="Ease in out"
               animation="fade up"
               easing="ease-in-out"
             />
-            <Card
+            <TheCard
               title="Ease out back"
               animation="fade up"
               easing="ease-out-back"
             />
-          </Section>
+          </TheSection>
 
-          <Section
+          <TheSection
             id="speed"
             title="Speed variations"
             description="Gotta go fast! Scales from the global duration."
           >
-            <Card
+            <TheCard
               title="Slow"
               animation="zoom-in fade up right blur"
               speed="slow"
             />
-            <Card
+            <TheCard
               title="Extra slow"
               animation="fade up left blur"
               speed="extra-slow"
             />
-            <Card
+            <TheCard
               title="Regular"
               animation="fade zoom-out"
               class="col-span-2"
             />
-            <Card
+            <TheCard
               title="Fast"
               animation="fade up right blur"
               speed="fast"
             />
-            <Card
+            <TheCard
               title="Extra fast"
               animation="fade up left blur"
               speed="extra-fast"
             />
-          </Section>
+          </TheSection>
           
-          <Section
+          <TheSection
             id="size"
             title="Size variations"
             description="Define the size of the animation. Scales from the global size."
           >
-            <Card
+            <TheCard
               title="Small"
               animation="fade right"
               size="small"
             />
-            <Card
+            <TheCard
               title="Medium"
               animation="fade left"
               size="medium"
             />
-            <Card
+            <TheCard
               title="Regular"
               animation="fade zoom-out"
               class="col-span-2"
             />
-            <Card
+            <TheCard
               title="Large"
               animation="fade right"
               size="large"
             />
-            <Card
+            <TheCard
               title="Extra large"
               animation="fade left"
               size="extra-large"
             />
-          </Section>
+          </TheSection>
 
-          <Section
+          <TheSection
             id="stagger"
             title="Stagger"
             description="Adjust delay for each item in a loop"
           >
             <div class="grid col-span-2 gap-2 lg:grid-cols-4">
-              <Card
+              <TheCard
                 v-for="n in 8"
                 :key="n"
-                :title="`Card ${n + 1}`"
+                :title="`TheCard ${n + 1}`"
                 class-name="min-h-[200px]"
                 animation="fade up"
                 :variables="{ delay: `${n}00ms` }"
               >
                 <p>{{ `--hs-delay: ${n}00ms` }}</p>
-              </Card>
+              </TheCard>
             </div>
-          </Section>
+          </TheSection>
 
-          <Section
+          <TheSection
             id="once"
             title="Once"
             description="Stays visible even with repeat or mirror enabled"
           >
-            <Card
+            <TheCard
               title="Once"
               animation="fade up once"
             />
-            <Card
+            <TheCard
               title="Not once"
               animation="fade up"
             />
-          </Section>
+          </TheSection>
 
-          <Section
+          <TheSection
             id="responsive"
             title="Responsive"
             description="Responsive animations based on breakpoints. Like any other TailwindCSS utility class. Default prefixes are sm:, md:, lg:, xl:, 2xl: "
           >
-            <Card
+            <TheCard
               title="Example 1"
               animation="fade left md:up"
             />
-            <Card
+            <TheCard
               title="Example 2"
               animation="fade up lg:down"
             />
-          </Section>
+          </TheSection>
         </div>
       </div>
     </div>
